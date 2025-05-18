7d7ffe668c1de0cb2fcef91da665f5e5c1d7e218
aaronamm <theeranitp@gmail.com>
2025-05-18 23:40:14 +0700

    Update Ping script

--- src/Modules/OrchardExample.Module/src/components/Ping.ts
+++ src/Modules/OrchardExample.Module/src/components/Ping.ts
@@ -1,21 +1,28 @@
 import * as signalR from '@microsoft/signalr';
 
+const connection = new signalR.HubConnectionBuilder()
+  .withUrl('/ping-hub', {})
+  .withAutomaticReconnect()
+  .build();
 
-export default async function ping() {
+connection.on('pong', async (message: string) => {
+  console.log(`Got a response message as ${message}`);
+});
 
-  const connection = new signalR.HubConnectionBuilder()
-    .withUrl('/ping-hub', {})
-    .withAutomaticReconnect()
-    .build();
-
-  connection.on('pong', async (message: string) => {
-    console.log(`Got a response message as ${message}`);
-  });
-
-
+async function startConnection() {
   await connection.start();
   console.log('connected');
-};
+}
+
+
+startConnection();
+
+export default function Ping() {
+  console.log('calling ping');
+}
+
+
+
 
 
 
