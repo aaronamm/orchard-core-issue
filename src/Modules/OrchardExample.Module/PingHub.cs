2daefc8926c6f80f18d363d7ffd0aedc431548a2
aaronamm <theeranitp@gmail.com>
2025-05-19 01:48:56 +0700

    Add IContentManager as dependency to reproduce an error

--- src/Modules/OrchardExample.Module/PingHub.cs
+++ src/Modules/OrchardExample.Module/PingHub.cs
@@ -1,22 +1,25 @@
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
+using OrchardCore.ContentManagement;
 
 namespace OrchardExample.Module;
 
 public class PingHub : Hub
 {
     private readonly ILogger _logger;
+    private readonly IContentManager _contentManager;
 
-    public PingHub(ILogger<PingHub> logger)
+    public PingHub(ILogger<PingHub> logger, IContentManager contentManager)
     {
         _logger = logger;
+        _contentManager = contentManager;
     }
 
     public override async Task OnConnectedAsync()
     {
         var connectionId = Context.ConnectionId;
         _logger.LogInformation("signalR connectId {connectionId}", connectionId);
-        await this.Clients.Caller.SendAsync("pong", "Pong pong....");
+        await Clients.Caller.SendAsync("pong", "Pong pong....");
     }
 
 }
