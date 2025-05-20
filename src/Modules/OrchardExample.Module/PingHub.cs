a710d65885600d5550491125960f9f25309d9d9b
aaronamm <theeranitp@gmail.com>
2025-05-20 19:28:27 +0700

    Change Isession as a dependency and be able to reproduce the same error.

--- src/Modules/OrchardExample.Module/PingHub.cs
+++ src/Modules/OrchardExample.Module/PingHub.cs
@@ -1,26 +1,26 @@
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 using OrchardCore.ContentManagement;
+using YesSql;
 
 namespace OrchardExample.Module;
 
 // How to reproduce an error:
 // Follow README.md to run the project.
 // Navigatge to https://localhost:5001 will execute signalR connection.start() in Ping.ts
-// This will request to PingHub which has ContentManager as dependency and cause an error message.
-// We don't need to do anything on contentManager, just make it as dependency of signalR Hub.
-// The error message is in App_Data/logs/
+// This will request to PingHub which has ISession as a dependency and cause an error message.
+// We don't need to do anything on a session instance, just make it as dependency of signalR Hub.
+// The error message is in App_Data/logs/ folder.
 
 public class PingHub : Hub
 {
     private readonly ILogger _logger;
-    private readonly IContentManager _contentManager;
+    private readonly ISession _session;
 
-    // If a user hasn't logged in, this contrctor is called two times.
-    public PingHub(ILogger<PingHub> logger, IContentManager contentManager)
+    public PingHub(ILogger<PingHub> logger, ISession session)
     {
         _logger = logger;
-        _contentManager = contentManager;
+        _session = session;
     }
 
 
